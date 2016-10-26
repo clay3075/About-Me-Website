3e4eee18734c1f01fa3b73cf3fa8509a3214b7cd
Ubuntu <ubuntu@ip-172-31-49-35.ec2.internal>
2016-10-26 21:42:02 +0000

    Did a little more work on the email portion.

--- website/server.js
+++ website/server.js
@@ -3,6 +3,12 @@ var app = express();
 
 var path = require('path');
 
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+	  extended: true
+})); 
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
@@ -10,7 +16,14 @@ app.get('/', function (req, res) {
 });
 
 app.post('/send_email', function (req, res) {
-  res.send('Sending!');
+  res.sendFile(path.join(__dirname, 'public/sendmail.html'));
+  var message = '';
+  message += 'First Name: ' + req.body.first_name + '\n';
+  message += 'Last Name: ' + req.body.last_name + '\n';
+  message += 'Email: ' + req.body.email + '\n';
+  message += 'Phone: ' + req.body.telephone + '\n\n';
+  message += 'Message: ' + req.body.telephone;
+  console.log(message);
 });
 
 app.listen(8080, function () {
