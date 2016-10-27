01fb3f8593b70cf3dec46167e97a28707c1fd18e
Ubuntu <ubuntu@ip-172-31-49-35.ec2.internal>
2016-10-27 22:09:00 +0000

    Working in textarea.js right now.

--- website/public/assets/js/textarea.js
+++ website/public/assets/js/textarea.js
@@ -1,6 +1,26 @@
+var entriesFull = false;
+
  $(document).ready(function () {
 	$('#editor').jqxEditor({
-		height: "300px",
-		width: '400px'
+		//height: "10%",
+		width: '100%'
 	});
  }); 
+
+$(document).ready(function () {
+	$('#contactform').jqxValidator({ rules: [
+		{ input: '#first_name', message: 'The name should be more than 3 characters!', acthon: 'keyup', rule: 'minLength=3' },
+		{ input: '#last_name', message: 'The name should be more than 3 characters!', acthon: 'keyup', rule: 'minLength=3' },
+		{ input: '#email', message: 'Invalid e-mail!', action: 'keyup', rule: 'email'}], theme: 'summer'
+	});
+
+	$('#contactform').submit(function(event) {
+		if (!entriesFull) {
+			event.preventDefault();	
+		} else {
+			entriesFull = false;
+		}
+	});
+});
+
+
