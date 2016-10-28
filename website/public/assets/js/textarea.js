127de1d24b4977a2dd84aad56dcf8fec6b50f547
Ubuntu <ubuntu@ip-172-31-49-35.ec2.internal>
2016-10-28 02:26:31 +0000

    Adding input handling to email form.

--- website/public/assets/js/textarea.js
+++ website/public/assets/js/textarea.js
@@ -1,4 +1,3 @@
-var entriesFull = false;
 
  $(document).ready(function () {
 	$('#editor').jqxEditor({
@@ -8,18 +7,27 @@ var entriesFull = false;
  }); 
 
 $(document).ready(function () {
+	//needed because initial value of editor was <div></div> for some reason
+	$('#editor').val('');
+
+	//set up validator from jqwidgets
 	$('#contactform').jqxValidator({ rules: [
-		{ input: '#first_name', message: 'The name should be more than 3 characters!', acthon: 'keyup', rule: 'minLength=3' },
-		{ input: '#last_name', message: 'The name should be more than 3 characters!', acthon: 'keyup', rule: 'minLength=3' },
-		{ input: '#email', message: 'Invalid e-mail!', action: 'keyup', rule: 'email'}], theme: 'summer'
+		{ input: '#first_name', message: 'The name should be more than 3 characters=', action: 'keyup', rule: 'minLength=3' },
+		{ input: '#last_name', message: 'The name should be more than 3 characters=', action: 'keyup', rule: 'minLength=3' },
+		{ input: '#email', message: 'Invalid e-mail=', action: 'keyup', rule: 'email'},
+		{ input: '#telephone', message: '(xxx)xxx-xxxx', action: 'keyup', rule: 'phone'},
+		{ input: '#editor', message: 'Tell me more please.', action: 'keyup', rule: 'minLength=10' }], theme: 'summer'
 	});
 
+	$('#contactform').jqxValidator({ onSuccess: function () { $(document).href += 'send_email'; console.log('hi');}});
+
+	//ensure form doesnt get submitted empty
 	$('#contactform').submit(function(event) {
-		if (!entriesFull) {
-			event.preventDefault();	
-		} else {
-			entriesFull = false;
-		}
+		//if ($('#first_name').val() === '' && $('#last_name').val() === '' && $('#email').val() === '' && $('#editor').val() === '') {
+			//event.preventDefault();
+		//}
+		event.preventDefault();
+		$('#contactform').jqxValidator('validate');
 	});
 });
 
